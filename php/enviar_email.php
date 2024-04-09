a854ca531dfcbec35b430475e99a022191f7fcf8
Gustavo Amaral Guimarães <gustavoamaralgui@gmail.com>
2024-04-09 07:55:20 -0300

    09/04/2024 07:55

--- php/enviar_email.php
+++ php/enviar_email.php
@@ -1,33 +1,40 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $assunto = $_POST['assunto'];
     $mensagem = $_POST['mensagem'];
     
-    $destinatario = "contato@gganalytics.com.br"; // Substitua pelo seu endereço de e-mail
+    // Configurar o objeto PHPMailer
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contato@gganalytics.com.br'; // Seu e-mail
+    $mail->Password = 'ObscureGBRTh100@'; // Sua senha
+    $mail->SMTPSecure = 'ssl'; // ssl ou tls
+    $mail->Port = 465; // Porta SMTP
+
+    // Configurar remetente e destinatário
+    $mail->setFrom($email);
+    $mail->addAddress('contato@gganalytics.com.br');
     
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-    
-    $mensagem_formatada = "
-    <html>
-    <head>
-    <title>Contato pelo Site</title>
-    </head>
-    <body>
-    <h2>Contato pelo Site</h2>
-    <p><strong>E-mail do remetente:</strong> $email</p>
-    <p><strong>Assunto:</strong> $assunto</p>
-    <p><strong>Mensagem:</strong> $mensagem</p>
-    </body>
-    </html>";
+    // Configurar o e-mail
+    $mail->isHTML(true);
+    $mail->Subject = $assunto;
+    $mail->Body = "<h3>Mensagem de $email</h3><p>$mensagem</p>";
     
     // Enviar e-mail
-    if (mail($destinatario, $assunto, $mensagem_formatada, $headers)) {
+    if ($mail->send()) {
         echo "E-mail enviado com sucesso!";
     } else {
-        echo "Erro ao enviar o e-mail.";
+        echo "Erro ao enviar o e-mail: " . $mail->ErrorInfo;
     }
 }
 ?>

eb60a1319b81ff218e50a8a08fbac618bcc157e6
Gustavo Amaral Guimarães <gustavoamaralgui@gmail.com>
2024-04-10 12:09:31 -0300

    12:09

--- js/script-animation.js
+++ js/script-animation.js
@@ -1,5 +1,18 @@
-AOS.init({
-    duration: 800, // Duração da animação
-    easing: 'ease-in-out', // Tipo de animação
-    once: true // Só anima uma vez
+window.addEventListener('scroll', function() {
+  var cards = document.querySelectorAll('.animacao');
+  cards.forEach(function(card) {
+    if (isElementInViewport(card)) {
+      card.classList.add('visible');
+    }
   });
+});
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
